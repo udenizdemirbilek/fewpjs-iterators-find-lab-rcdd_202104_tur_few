87a57bc0ad066c157a2fec4e50d0089505bdd3d6
demirbileku <udemirbilek2@gmail.com>
2021-05-15 23:29:25 +0300

    commit tree cc865389dfdaa44319691f596c44b8f43e36bbe3

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function wonSuperbowl (game){
+//   if (game.result === "W"){
+//     return game.year
+//   }  
+// }
+function superbowlWin (array){
+  const output = array.find(({result}) => result === "W")
+  if (output != undefined){
+	return output.year
+  }
+}
